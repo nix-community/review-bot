3c0641d5ee391e15f2b10f089d224ed8ed687016
ckie <git-525ff67@ckie.dev>
2022-05-04 15:47:11 +0300

    pr: configure git user.{email,name}

--- src/commands/pr.ts
+++ src/commands/pr.ts
@@ -49,9 +49,10 @@ ${prInfoText}`,
 
 
 async function runNixpkgsReview(pr: number, githubToken: string) {
-    const nprBinPath = await findBinariesInPath("nixpkgs-review");
+    const nprBin = await findBinariesInPath("nixpkgs-review")[0];
+    const gitBin = await findBinariesInPath("git")[0];
 
-    const child = spawn(`${nprBinPath[0]} pr --no-shell --sandbox --post-result ${pr}`,
+    const child = spawn(`${gitBin} config user.email review-bot@example.com && ${gitBin} config user.name review-bot && ${nprBin} pr --no-shell --sandbox --post-result ${pr}`,
         {
             env: { GITHUB_TOKEN: githubToken },
             cwd: config.nixpkgsRepo.localPath,
