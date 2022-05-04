c3507bbcc3957bc8b2cccc72b62c20771f6ae678
ckie <git-525ff67@ckie.dev>
2022-05-04 13:45:33 +0300

    index: remove useless comments

--- src/index.ts
+++ src/index.ts
@@ -9,30 +9,17 @@ import * as path from "path";
 import config from "./config";
 import CommandHandler from "./commands/handler";
 
-// First things first: let's make the logs a bit prettier.
 LogService.setLogger(new RichConsoleLogger());
-
-// For now let's also make sure to log everything (for debugging)
 LogService.setLevel(LogLevel.DEBUG);
 
-// Print something so we know the bot is working
-LogService.info("index", "Bot starting...");
-
-
-// This is the startup closure where we give ourselves an async context
 (async function () {
-    // Prepare the storage system for the bot
     const storage = new SimpleFsStorageProvider(path.join(config.dataPath, "bot.json"));
-
-    // Now create the client
     const client = new MatrixClient(config.homeserverUrl, config.accessToken, storage);
 
-    // Setup the autojoin mixin (if enabled)
     if (config.autoJoin) {
         AutojoinRoomsMixin.setupOnClient(client);
     }
 
-    // Prepare the command handler
     const commands = new CommandHandler(client);
     await commands.start();
 
